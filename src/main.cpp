fcd0d320adbadbd65fb418afde3f146244c1c60c
Kohmei Kadoya <kkadoya@wpi.edu>
2020-11-09 23:31:58 -0500

    Intitial Commit for WIll's protoype
    Cubic tank drive, Bumper buttons for intake control

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       kohmeikadoya                                              */
+/*    Created:      Mon Nov 09 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+  //All the control code is in robot config, that seems to be the new template
+  //Auton and other non-controller stuff can go here
+
+}
---
+++ src/robot-config.cpp
@@ -0,0 +1,124 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor leftMotorA = motor(PORT1, ratio18_1, false);
+motor leftMotorB = motor(PORT2, ratio18_1, false);
+motor leftMotorC = motor(PORT3, ratio18_1, false);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB, leftMotorC);
+
+motor rightMotorA = motor(PORT4, ratio18_1, true);
+motor rightMotorB = motor(PORT5, ratio18_1, true);
+motor rightMotorC = motor(PORT6, ratio18_1, true);
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB, rightMotorC);
+
+motor leftIntake = motor(PORT7, ratio18_1, false);
+motor rightIntake = motor(PORT8, ratio18_1, true);
+
+motor rollerFront = motor(PORT9, ratio18_1, false);
+motor rollerBack = motor(PORT11, ratio18_1, false);
+
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 4*3.14, 11, 7, inches, 1);
+controller Controller1 = controller(primary);
+
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3
+      // right = Axis2
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position()^3;
+      int drivetrainRightSideSpeed = Controller1.Axis2.position()^3;
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor next time the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+
+    if(Controller1.ButtonL1.pressing()){
+      leftIntake.spin(forward, 50, percentUnits::pct);
+      rightIntake.spin(forward, 50, percentUnits::pct);
+    }else if (Controller1.ButtonL2.pressing()) {
+      leftIntake.spin(reverse, 50, percentUnits::pct);
+      rightIntake.spin(reverse, 50, percentUnits::pct);
+    }else{
+      leftIntake.stop(brakeType::coast);
+      rightIntake.stop(brakeType::coast);
+    }
+
+    if(Controller1.ButtonR1.pressing()){
+      rollerFront.spin(forward, 50, percentUnits::pct);
+      rollerBack.spin(forward, 50, percentUnits::pct);
+    }else if (Controller1.ButtonR2.pressing()) {
+      rollerFront.spin(reverse, 50, percentUnits::pct);
+      rollerBack.spin(reverse, 50, percentUnits::pct);
+    }else{
+      rollerFront.stop(brakeType::coast);
+      rollerBack.stop(brakeType::coast);
+    }
+
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+}
